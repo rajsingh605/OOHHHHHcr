d163ca8312ccbc64611a929e9bd5066e24e23ce5
rajsingh605 <rajsingh446@gmail.com>
2016-05-12 13:02:54 +0530

    btp tayaar hone hi wala hai

--- app/src/main/java/example/raj/oohhhhhcr/MainActivity.java
+++ app/src/main/java/example/raj/oohhhhhcr/MainActivity.java
@@ -2,29 +2,39 @@ package example.raj.oohhhhhcr;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int SELECT_FILE = 1;
     private static final int REQUEST_CAMERA = 2;
     private static final int PIC_CROP = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,25 +95,21 @@ public class MainActivity extends AppCompatActivity {
                     ivImage.setImageBitmap(thumbnail);
             }
             else if (requestCode == SELECT_FILE) {
-                Uri selectedImageUri = data.getData();
-                String[] projection = {MediaStore.MediaColumns.DATA};
-                Cursor cursor = getContentResolver().query(selectedImageUri, projection, null, null, null);
-                int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
                 cursor.moveToFirst();
-                String selectedImagePath = cursor.getString(column_index);
-                Bitmap bm;
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                //options.inJustDecodeBounds = true;
-                BitmapFactory.decodeFile(selectedImagePath, options);
-                final int REQUIRED_SIZE = 200;
-                int scale = 1;
-                while (options.outWidth / scale / 2 >= REQUIRED_SIZE && options.outHeight / scale / 2 >= REQUIRED_SIZE)
-                    scale *= 2;
-                options.inSampleSize = scale;
-                //options.inJustDecodeBounds = false;
-                bm = BitmapFactory.decodeFile(selectedImagePath, options);
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                Bitmap bm = null;
+                try {
+                    bm = BitmapFactory.decodeStream(getContentResolver().openInputStream(selectedImage));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
                 if (ivImage != null)
-                    ivImage.setImageURI(selectedImageUri);
+                    ivImage.setImageBitmap(bm);
                 //TessDataManager.initTessTrainedData(context);
                 String Data_Path = context.getFilesDir().toString() + "/OCR/";
                 String tessPath = context.getFilesDir().toString() + "/OCR/tessdata/";
@@ -116,6 +122,22 @@ public class MainActivity extends AppCompatActivity {
                 if (!dir.exists()) {
                     if(!dir.mkdirs())
                         System.out.println("Failed");
+                }
+                if (!(new File(Data_Path + "tessdata/" + "eng" + ".traineddata")).exists()) {
+                    try {
+                        AssetManager assetManager = getAssets();
+                        InputStream in = assetManager.open("tessdata/" + "eng" + ".traineddata");
+                        OutputStream out = new FileOutputStream(Data_Path + "tessdata/" + "eng" + ".traineddata");
+                        byte[] buf = new byte[1024];
+                        int len;
+                        while ((len = in.read(buf)) > 0) {
+                            out.write(buf, 0, len);
+                        }
+                        in.close();
+                        out.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
                 TessBaseAPI baseApi = new TessBaseAPI();
                 baseApi.setDebug(true);
